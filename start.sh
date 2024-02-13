ab5549a84b39371d39447b3c6cfb6a784bfe7d5f
Adam <wild@lerndmina.dev>
2024-02-13 11:54:22 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,11 @@ while true; do
     if [ -f $FILE ]; then
         echo "$FILE found, starting Bot..."
         echo "Bot started @ $(date)" >>restart.log
-        rm -r $REMOVE && git pull && yarn && echo "Booting bot output sent to $LOGFILE" && yarn start
+        if [ -d $REMOVE ]; then
+            echo "Removing old dist/ folder..."
+            rm -rf $REMOVE
+        fi
+        git pull && yarn && echo "Booting bot output sent to $LOGFILE" && yarn start
         echo "Bot detected in a crashed or stopped state, beginning restart process..."
         echo "Bot stoped or crashed @ $(date)" >>restart.log
         echo "Restarting in 3 seconds"

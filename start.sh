d5703b32b6d6bdab12a9407109c6c1a99311a190
Adam <wild@lerndmina.dev>
2024-02-13 11:51:47 +0000

    Remove dist before run

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 FILE="src/Bot.ts"
+REMOVE="dist/"
 LOGFILE="bot.log"
 while true; do
     if [ -f $FILE ]; then
         echo "$FILE found, starting Bot..."
         echo "Bot started @ $(date)" >>restart.log
-        git pull && yarn && echo "Booting bot output sent to $LOGFILE" && yarn start
+        rm -r $REMOVE && git pull && yarn && echo "Booting bot output sent to $LOGFILE" && yarn start
         echo "Bot detected in a crashed or stopped state, beginning restart process..."
         echo "Bot stoped or crashed @ $(date)" >>restart.log
         echo "Restarting in 3 seconds"

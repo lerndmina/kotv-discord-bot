77759d9ce495895072cb42c3b48f08a808afb493
Adam <wild@lerndmina.dev>
2024-03-19 14:58:11 +0000

    Require ManagMessages permission to create a poll

--- src/commands/utilities/poll.ts
+++ src/commands/utilities/poll.ts
@@ -54,6 +54,7 @@ export const data = new SlashCommandBuilder()
 export const options: CommandOptions = {
   devOnly: false,
   deleted: false,
+  userPermissions: ["ManageMessages"],
 };
 
 export async function run({ interaction, client, handler }: SlashCommandProps) {
